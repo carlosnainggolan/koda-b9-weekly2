6f160ec4da4e79ee59fad332db9275a1b56e709c
carlos <carlosnainggolan@gmail.com>
2026-07-23 19:58:20 +0700

    feat: making getDataFromServer

---
+++ src/soal5.js
@@ -0,0 +1,15 @@
+const getDataFromServer = (status, callback) => {
+  if (status) {
+    setTimeout(() => {
+      const products = [
+        'Product 1',
+        'Product 2',
+        'Product 3'
+      ]
+      callback(products, null)
+    }, 3000);
+  } else {
+    const err = new Error('Failed to fetch data')
+    callback(null,err)
+  }
+}
